e8566a49191472015e93262b71628eff5b83eaf3
Andreas Andreou <aandreou.cs@gmail.com>
2015-08-25 19:04:15 +0300

    Add Minimap

---
+++ Unity_Project/Base_Project/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIManager : MonoBehaviour {
+
+	public RenderTexture minimapTexture;
+	public Material minimapMaterial;
+
+	public Vector2 minimapOffset;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+
+	}
+
+	void OnGUI ()
+	{
+		Rect minimapTextureRect = new Rect (Screen.width - minimapTexture.width - minimapOffset.x, minimapOffset.y, minimapTexture.width, minimapTexture.height);
+		Graphics.DrawTexture (minimapTextureRect, minimapTexture, minimapMaterial);  
+	}
+
+}
---
+++ Unity_Project/Base_Project/Assets/Scripts/Minimap/MinimapCameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapCameraMovement : MonoBehaviour {
+
+	//target is main camera
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.position = new Vector3 (target.position.x, transform.position.y, target.position.z);
+
+	}
+}
